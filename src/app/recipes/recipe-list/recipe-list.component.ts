bb4380e873b28944be535e24c69d7ef6b34adaab
Tushar Dave <tushar.dave@dotsquares.com>
2019-03-30 18:13:35 +0530

    added all files initially

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipes} from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes : Recipes[] = [
+  new Recipes('first receipe','recipe description','https://upload.wikimedia.org/wikipedia/commons/2/2f/Culinary_fruits_front_view.jpg'),
+  new Recipes('second receipe','recipe description','https://cdn.pixabay.com/photo/2017/06/02/18/24/fruit-2367029_960_720.jpg'),
+
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
