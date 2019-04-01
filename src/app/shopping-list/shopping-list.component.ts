8022bc54eab093fa94551e3d52c09c8df519716d
Tushar Dave <tushar.dave@dotsquares.com>
2019-04-01 19:19:16 +0530

    added extra files

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Ingredient} from '../shared/ingredients.model'
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients:Ingredient[] = [
+  			new Ingredient('apples',5),
+  			new Ingredient('oranges',10),
+  ];
   constructor() { }
 
   ngOnInit() {
